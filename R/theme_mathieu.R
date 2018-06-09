1a2e0df5266baef86c0a670de519c53e6ca5aa09
mathieubray <braymath@umich.edu>
2018-06-09 12:09:15 -0400

    Improved Flexibility

--- R/theme_mathieu.R
+++ R/theme_mathieu.R
@@ -2,52 +2,52 @@
 #'
 #' Custom ggplot2 themes for plots
 #'
-#' @param base_size Text size
-#' @param base_family Text font
 #' @param title.label Title
-#' @param subtitle.label Subtitle
 #' @param x.label X Label
 #' @param y.label Y Label
-#' @param x.watermark X coordinate for watermark
-#' @param y.watermark Y coordinate for watermark
+#' @param subtitle.label Subtitle
+#' @param base_size Font size
+#' @param watermark.coord Coordinates for watermark
 #' @param watermark.size Size of the watermark
+#' @param watermark.alpha Transparency of the watermark
+#' @param watermark.angle Angle of the watermark
+#' @param watermark.color Color of the watermark
 #'
 #' @return The ggplot2 theme
 #' @export
 #'
 #' @examples
-#' x.annot <- mean(player.bios$Height)
-#' y.annot <- mean(player.bios$Weight)
-#'
-#' ggplot(player.bios,aes(x=Position,y=Weight)) +
-#'  geom_boxplot() +
-#'  theme_mathieu(title.label="NHL Height by Position",
-#'                subtitle.label="A comparison of the heights of skaters who played in the NHL",
-#'                x.label="Position",
-#'                y.label="Weight",
-#'                x.watermark=2.5,
-#'                y.watermark=y.annot,
-#'                watermark.size=10)
+#' ggplot(mtcars, aes(x=wt,y=qsec))
+#' + geom_point() +
+#' theme_mathieu(title.label="mathieuR Theme Example",
+#' subtitle.label="Compares wt and qsec of the mtcars dataset",
+#' x.label = "Weight",
+#' y.label = "QSEC",
+#' watermark.coord=c(mean(mtcars$wt), mean(mtcars$qsec)))
 
-theme_mathieu <- function(base_size=14,base_family="",
-                          title.label, subtitle.label, x.label, y.label,
-                          x.watermark=NA, y.watermark=NA, watermark.size=NA){
+theme_mathieu <- function(title.label,
+                          x.label="",
+                          y.label="",
+                          subtitle.label="",
+                          base_size=14,
+                          watermark.coord=NULL,
+                          watermark.size=15,
+                          watermark.alpha=0.15,
+                          watermark.angle=30,
+                          watermark.color="red"){
 
-  if (!is.na(x.watermark) | !is.na(y.watermark)){
-    if (is.na(watermark.size)){
-      watermark.size <- 20
-    }
+  if (!is.null(watermark.coord)){
     watermark <- paste0("@mathieubray ",lubridate::year(lubridate::today()))
   } else {
     watermark <- ""
   }
 
-  list(ggplot2::theme_bw(base_size, base_family),
+  list(ggplot2::theme_bw(base_size = base_size),
        ggplot2::ggtitle(title.label, subtitle=subtitle.label),
        xlab(x.label),
        ylab(y.label),
-       ggplot2::annotate("text",x=x.watermark,y=y.watermark,col="red",label=watermark,
-             alpha=0.15,cex=watermark.size,fontface="bold",angle=30)
+       ggplot2::annotate("text",x=watermark.coord[1],y=watermark.coord[2],col=watermark.color,label=watermark,
+             alpha=watermark.alpha,cex=watermark.size,fontface="bold",angle=watermark.angle)
   )
 
 }
